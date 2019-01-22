9c51a4528a50d62db832dceb0a6663c5ac2f7e12
Carsten Heinrigs <carsten@cahein.de>
2019-01-22 14:26:19 +0100

    related to rails upgrade

--- app/controllers/application_controller_decorator.rb
+++ app/controllers/application_controller_decorator.rb
@@ -1,8 +1,7 @@
 ApplicationController.class_eval do
-  before_filter :set_current_user
+  before_action :set_current_user
 
   def set_current_user
     Spree::User.current = spree_current_user
   end
 end
-
--- db/migrate/20110331232151_usergroup_variant.rb
+++ db/migrate/20110331232151_usergroup_variant.rb
@@ -1,14 +1,14 @@
-class UsergroupVariant < ActiveRecord::Migration
+class UsergroupVariant < ActiveRecord::Migration[4.2]
   def self.up
-	      create_table :spree_user_groups_variants do |t|
-		      t.integer :user_group_id
-		      t.integer :variant_id
-		      t.decimal :price, :precision => 8, :scale => 2
-		      t.timestamps
-	    end
+    create_table :spree_user_groups_variants do |t|
+      t.integer :user_group_id
+      t.integer :variant_id
+      t.decimal :price, :precision => 8, :scale => 2
+      t.timestamps
+    end
   end
 
   def self.down
-	  drop_table :spree_user_groups_variants
+    drop_table :spree_user_groups_variants
   end
 end
